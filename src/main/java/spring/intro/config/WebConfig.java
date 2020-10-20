b1275f61f2c07fb33bcc7c0a14d5499804849a1a
Kostian <pyteec.ua@i.ua>
2020-10-20 18:03:14 +0300

    worked with hello controller and with JSP

---
+++ src/main/java/spring/intro/config/WebConfig.java
@@ -0,0 +1,23 @@
+package spring.intro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"spring.intro.controller"})
+public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        InternalResourceViewResolver resolve = new InternalResourceViewResolver();
+        resolve.setViewClass(JstlView.class);
+        resolve.setPrefix("/WEB-INF/views/");
+        resolve.setSuffix(".jsp");
+        return resolve;
+    }
+}
