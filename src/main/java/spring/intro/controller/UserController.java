0a835e053329ae0007aacf66611e846d9dabead0
Kostian <pyteec.ua@i.ua>
2020-10-21 12:29:10 +0300

    deleted unnecessary dependency in pom.xml file and used  constructor in UserController class

--- src/main/java/spring/intro/controller/UserController.java
+++ src/main/java/spring/intro/controller/UserController.java
@@ -3,12 +3,11 @@ package spring.intro.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.apache.log4j.Logger;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import spring.intro.config.AppConfig;
 import spring.intro.dto.UserResponseDto;
 import spring.intro.model.User;
 import spring.intro.service.UserService;
@@ -17,9 +16,12 @@ import spring.intro.service.UserService;
 @RequestMapping("/user")
 public class UserController {
     private static final Logger logger = Logger.getLogger(UserController.class);
-    AnnotationConfigApplicationContext context
-            = new AnnotationConfigApplicationContext(AppConfig.class);
-    UserService userService = context.getBean(UserService.class);
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/inject")
     public void inject() {
